8e31a3539074da76fe9567f74e85729b3bc4c146
xiezongchen <952937570@qq.com>
2021-06-13 10:37:32 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Demo/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Demo from './components/Demo.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Demo
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="content">
+    <n-card class="card">
+      <n-form>
+        <n-form-item>
+          <n-input
+            v-model:value="value"
+            type="textarea"
+            placeholder="请输入"
+            :autosize="{ minRows: 2, maxRows: 5 }"
+          />
+        </n-form-item>
+      </n-form>
+    </n-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+import { NInput, NCard, NFormItem, NForm } from 'naive-ui';
+export default defineComponent({
+  name: 'Demo',
+  components: {
+    NInput,
+    NCard,
+    NForm,
+    NFormItem,
+  },
+  setup: () => {
+    return {
+      value: ref(''),
+    };
+  },
+});
+</script>
+
+<style scoped>
+.content {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.card {
+  width: 30%;
+}
+</style>
